e4e11a23dad78f42db58ddefd9487a96c7f60ba2
InvestorSite <64286901+IrciaF@users.noreply.github.com>
2021-02-13 22:34:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
 var helloSpeaker = {};
-  var byeSpeakerr = {};
+  var byeSpeaker = {};
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var i = 0; i < names.length; i++) {
     var firstLetter = names[i].charAt(0).toLowerCase();
